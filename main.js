2ee37802840f133dd09d4c963b5a8cb894586124
Valentin <20223tn029@utez.edu.mx>
2025-09-25 14:08:52 -0600

    Cambio en las rutas

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ function logStatus(status) {
 // Registro del Service Worker
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js').then(registration => {
+        navigator.serviceWorker.register('./sw.js').then(registration => {
             console.log('Service Worker registrado con éxito:', registration);
 
             // Escuchar el estado de instalación y activación
